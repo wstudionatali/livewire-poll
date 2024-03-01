1a7a1eb9ccb87b9f81ee1228c64e4d0379ceb5f3
wstudionatali <wstudionatali@gmail.com>
2024-03-02 00:51:28 +0100

    Livewire Actions

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,8 +7,19 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = ['First'];
     public function render()
     {
         return view('livewire.create-poll');
     }
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        $this->options = array_values($this->options);
+    }
+
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -2,8 +2,24 @@
 <form>
     <label>Poll Title</label>
 
-    <input type="text" wire:model.live="title" />
+    <input type="text" wire:model.debounce.200ms="title" />
 
     Current title: {{ $title }}
+    <div class="mb-4 mt-4">
+      <button class="btn" wire:click.prevent="addOption">Add Option</button>
+    </div>
+
+    <div>
+      @foreach ($options as $index => $option)
+        <div class="mb-4">
+        <label>Option {{ $index + 1 }}</label>
+          <div class="flex gap-2">
+            <input type="text" wire:model="options.{{ $index }}" />
+            <button class="btn"
+              wire:click.prevent="removeOption({{ $index }})">Remove</button>
+          </div>
+        </div>
+      @endforeach
+    </div>
   </form>
 </div>
